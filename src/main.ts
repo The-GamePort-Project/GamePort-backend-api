a8920a7d4ae5fa3c8ef784d50116a58a52cabdb4
doctorbond2 <michael.rostlund@hotmail.com>
2025-04-14 15:32:23 +0200

    added prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//abort on error maybe?
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Error during application bootstrap:', error);
+});
